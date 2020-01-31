cc84bd6b7f0b1a14228ac5474b5bfbfe2dc7d523
jab_tx <javier.a.baragan92@gmail.com>
2020-01-30 19:14:38 -0600

    Added start button to Quiz Page which starts the quiz with full functionality

--- script.js
+++ script.js
@@ -1,38 +1,56 @@
-var timeEl = document.querySelector(".time");
+var nextButton = document.getElementById('Next-btn').style.visibility = "hidden";
+var startButton = document.getElementById('quizStart-btn')
+// var questionEl = document.getElementById('question');
+var answerButtonsElement = document.getElementById('answer-buttons');
 
-var secondsLeft = 90;
-const startButton= document.getElementById('start-btn');
+var currentQuestionsIndex;
 
-function setTime() {
+
+startButton.addEventListener('click', quizStart);
+
+
+function quizStart(){
+  document.getElementById("quizStart-btn").style.visibility = "hidden";
+  document.getElementById("Next-btn").style.visibility = "visible";
+  timeSet();
+  showQuestion();
+};
+
+function timeSet(){
+  var secondsLeft = 90;
+  var timeEl = document.querySelector(".time");
     var timerInterval = setInterval(function() {
       secondsLeft--;
       timeEl.textContent = secondsLeft + " SECONDS LEFT!";
-  
       if(secondsLeft === 0) {
         clearInterval(timerInterval);
-        sendMessage();
       }
-  
     }, 1000);
-  }
-
-
-// nextQuestion()
-
-
-// function nextQuestion(){}
+};
 
 
 // function selectAnswer(){}
 
 
-// const questions = [
+
+var questions = [
+{
+
+  question: 'What color is the sky?',
+  answers:[
+
+    { text: 'Blue', correct: true},
+    { text: 'Green', correct: false}
+
+          ]
 
 
 
+}  
+];
 
 
-    
-// ]
+function showQuestion(){
+  question.innerText = questions;
 
-setTime();
+}
